7426d04b46f82445e5f263d5476667db4b5e29ec
sohan-gts <sohan@gtsinfosoft.com>
2022-12-28 23:31:43 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,97 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import {MatTableDataSource} from '@angular/material/table';
+import  { dummyData }   from "./data";
+import {MatPaginator} from '@angular/material/paginator';
+import { MatSort, Sort } from '@angular/material/sort';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+export interface dummy {
+  name: string;
+  creation_date: Date;
+  type: number;
+}
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ngTask';
+export class AppComponent implements AfterViewInit{
+
+  inputVal!:string;
+  typeVal!:string;
+  typeFilter: any = {
+    1:'User',
+    2:'Admin'
+  }
+
+  range = new FormGroup({
+    start: new FormControl(null,[Validators.required]),
+    end: new FormControl(null,[Validators.required]),
+  });
+
+  displayedColumns: string[] = ['name', 'creation_date', 'type'];
+  dataSource = new MatTableDataSource(dummyData);
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  
+
+  constructor(){
+
+  }
+
+  
+  applyFilter(event: Event) : void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  ngAfterViewInit():void {
+    console.log(this.sort);
+    
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+
+  
+  clearSearch(input:HTMLInputElement) : void {
+    input.value = '';
+    this.dataSource.filter = '';
+  }
+
+
+
+  tableFilter():void{
+    
+    this.dataSource.data = dummyData;
+    
+    if(this.typeVal && this.range.valid){
+     
+      this.dataSource.data = this.dataSource.data.filter(e=> {
+     return  e.type === Number(this.typeVal) && 
+        e.creation_date.getTime() >= this.range.get('start')?.value.getTime() && 
+        e.creation_date.getTime() <= this.range.get('end')?.value.getTime() 
+      });
+
+    }else if(this.range.valid){
+      this.dataSource.data = this.dataSource.data.filter(e=> (
+        e.creation_date.getTime() >= this.range.get('start')?.value.getTime() && 
+           e.creation_date.getTime() <= this.range.get('end')?.value.getTime() ));
+    }else if (this.typeVal)
+    this.dataSource.data = this.dataSource.data.filter(e=> (e.type === Number(this.typeVal) ));
+
+  }
+
+  clearAll(input:HTMLInputElement):void{
+    this.range.reset(); 
+    this.clearSearch(input);
+    this.typeVal = '';
+    this.tableFilter()
+  }
+
+  openGithub():void{
+    location.href='https://github.com/sohanpaliyal'
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatSelectModule} from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
@@ -10,7 +24,22 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatTooltipModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
